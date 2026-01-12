ea71b5a116a39289787e5e1f264affeea2439aef
gusmartinuk <gusmartinuk@gmail.com>
2026-01-12 22:57:22 +0000

    chore: lower min group count

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ DATABASE_URL = os.getenv(
 )
 ENV = os.getenv("ENV", "local")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
-MIN_GROUP_COUNT = int(os.getenv("MIN_GROUP_COUNT", "5"))
+MIN_GROUP_COUNT = int(os.getenv("MIN_GROUP_COUNT", "2"))
 MAX_DATE_RANGE_YEARS = int(os.getenv("MAX_DATE_RANGE_YEARS", "10"))
 DEFAULT_NO_LOCATION_MONTHS = int(os.getenv("DEFAULT_NO_LOCATION_MONTHS", "12"))
 CACHE_MAX_AGE = int(os.getenv("CACHE_MAX_AGE", "300"))
