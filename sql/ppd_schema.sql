aa6e31c9ae6e7dc0d080ad74fe795ecf4a27c534
gusmartinuk <gusmartinuk@gmail.com>
2026-01-13 18:48:40 +0000

    chore: optimise ppd field lengths based on observed data

--- sql/ppd_schema.sql
+++ sql/ppd_schema.sql
@@ -1,18 +1,20 @@
+-- Field lengths based on observed MSSQL max lengths with safety margin (2026-01).
+-- If future PPD schema changes, re-run max-length analysis before altering.
 CREATE TABLE ppd (
     transaction_id UUID NOT NULL,
     price BIGINT NOT NULL,
     date_of_transfer DATE NOT NULL,
-    postcode VARCHAR(10),
+    postcode VARCHAR(8),
     property_type CHAR(1),
     old_new CHAR(1),
     duration CHAR(1),
-    paon VARCHAR(300),
-    saon VARCHAR(300),
-    street VARCHAR(300),
-    locality VARCHAR(300),
-    town_city VARCHAR(300),
-    district VARCHAR(300),
-    county VARCHAR(300),
+    paon VARCHAR(120),
+    saon VARCHAR(80),
+    street VARCHAR(80),
+    locality VARCHAR(60),
+    town_city VARCHAR(60),
+    district VARCHAR(80),
+    county VARCHAR(60),
     ppd_category_type CHAR(1),
     record_status CHAR(1),
     CONSTRAINT pk_ppd PRIMARY KEY (transaction_id)
